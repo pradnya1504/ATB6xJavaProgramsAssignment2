ed70031f30cfaa4a79d228fecc900f31c7d49acc
pradnya1504 <165540609+pradnya1504@users.noreply.github.com>
2024-05-06 14:22:33 +0530

    Add files via upload

---
+++ src/JavaPrograms/Lab017.java
@@ -0,0 +1,24 @@
+package JavaPrograms;
+
+public class Lab017 {
+    public static void main(String[] args) {
+        int[][] arr_num1 = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        boolean present = false;
+        for (int i=0;i<arr_num1.length;i++) {
+            for (int j = 0; j < arr_num1[i].length; j++) {
+                if (arr_num1[i][j] == 9) {
+                    present = true;
+                    break;
+                }
+            }
+            if (present) {
+                break;
+            }
+        }
+            System.out.println(present);
+                }
+            }
---
+++ src/JavaPrograms/Lab018.java
@@ -0,0 +1,20 @@
+package JavaPrograms;
+
+public class Lab018 {
+    public static void main(String[] args) {
+        int[][] arr_num3 = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (arr_num3[i][j] == arr_num3[j][j]) {
+                }
+                    System.out.print(arr_num3[j][i]);
+            }
+            System.out.println("");
+        }
+    }
+}
+
